1b3e3cdc226375a57b50e6ceb56f0d3d8b6ee5b1
Bruno Rijsman <brunorijsman@gmail.com>
2019-09-23 16:46:25 +0000

    Finish teleportation example

--- lesson-2/teleportation.py
+++ lesson-2/teleportation.py
@@ -1,4 +1,7 @@
 import netsquid as ns
+import random
+
+VERBOSE = False
 
 STATE_ZERO = "|0>"
 STATE_ONE = "|1>"
@@ -15,30 +18,48 @@ ALL_STATES = [
     STATE_PLUS_I,
     STATE_MINUS_I]
 
-def pick_random_state_description():
-    return random.choice(ALL_STATES)
+def report(message):
+    if VERBOSE:
+        print(message)
 
-def create_qubit_for_chose_state(state):
+def create_random_state_and_qubit():
+    state = random.choice(ALL_STATES)
     q, = ns.qubits.create_qubits(1)
     if state == STATE_ZERO:
         pass
     elif state == STATE_ONE:
-        ns.qubits.operate(q, ns.NOT)
+        ns.qubits.operate(q, ns.X)
     elif state == STATE_PLUS:
-        ns.qubits.operate(q, ns.H)
+        ns.qubits.operate(q, ns.Ry90)
     elif state == STATE_MINUS:
-        ns.qubits.operate(q, ns.NOT)
-        ns.qubits.operate(q, ns.H)
+        ns.qubits.operate(q, ns.Ry270)
     elif state == STATE_PLUS_I:
-        pass
+        ns.qubits.operate(q, ns.Rx270)
     elif state == STATE_MINUS_I:
-        pass
-        
+        ns.qubits.operate(q, ns.Rx90)
+    else:
+        assert False, "Unexpected values for state"
+    return state, q
 
-
-def create_two_identical_random_qubits():
-    q1, q2 = ns.qubits.create_qubits(2)
-    return q1, q2, "|0>"
+def qubits_has_state(q, state):
+    # Note: this is impossible to check in real life, but we are doing a simulation, so we can peek
+    # under the hood.
+    if state == STATE_ZERO:
+        expected_ket_state = ns.qubits.ketstates.s0
+    elif state == STATE_ONE:
+        expected_ket_state = ns.qubits.ketstates.s1
+    elif state == STATE_PLUS:
+        expected_ket_state = ns.qubits.ketstates.h0
+    elif state == STATE_MINUS:
+        expected_ket_state = ns.qubits.ketstates.h1
+    elif state == STATE_PLUS_I:
+        expected_ket_state = ns.qubits.ketstates.y0
+    elif state == STATE_MINUS_I:
+        expected_ket_state = ns.qubits.ketstates.y1
+    else:
+        assert False, "Unexpected values for state"
+    fidelity = ns.qubits.fidelity(q, expected_ket_state)
+    return fidelity > 0.9999
 
 def create_phi_plus_bell_pair():
     q1, q2 = ns.qubits.create_qubits(2)
@@ -65,51 +86,34 @@ def bell_state_correction(q, b1, b2):
     else:
         assert False, "Unexpected values for classical bits"
 
-def qubits_have_same_state(q1, q2):
-    # Note: this is impossible to check in real life, but we are doing a simulation, so we can peek
-    # under the hood.
-    print(f"q2.qstate = {q2.qstate}")
-    fidelity = ns.qubits.fidelity(q1, q2.qstate)
-    print(f"fidelity: {fidelity}")
-    return fidelity > 0.999
-
 def teleport_one_qubit():
-    # Create a random source qubit. Also create a copy of this random source qubit to have something
-    # to compare the destination qubit with after the teleportation is complete, to check whether
-    # the teleportation was succesful. Note that we are not violating the no-cloning theorem here:
-    # we are not really copying any qubits, we are simply preparing two qubits in exactly the same
-    # way.
-    source_qubit, source_qubit_copy, source_qubit_description = create_two_identical_random_qubits()
-    print(f"source qubit: {source_qubit.qstate}")
-    print(f"source qubit copy: {source_qubit_copy.qstate}")
-    print(f"source qubit description: {source_qubit_description}")
-
+    # Pick a random state from a limited set of candidate states (ALL_STATES), remember what state
+    # we chose, and create a source qubit in that state.
+    source_state, source_qubit = create_random_state_and_qubit()
     # Create a |Φ+> Bell pair. One qubit is used as the destination qubit, and the other as a
     # helper qubit.
     helper_qubit, destination_qubit = create_phi_plus_bell_pair()
-    print(f"destination qubit: {destination_qubit.qstate}")
-    print(f"helper qubit: {helper_qubit.qstate}")
-
     # Perform a Bell State Measurement (BSM) on the source and helper qubits. This yields two
     # classical bits b1 and b2.
     b1, b2 = bell_state_measurement(source_qubit, helper_qubit)
-    print("After Bell State Measurement:")
-    print(f"classical bits b1 b2: {b1} {b2}")
-    print(f"source qubit: {source_qubit.qstate}")
-    print(f"helper qubit: {helper_qubit.qstate}")
-
     # Perform a Bell State Correction on the destination qubit, based on the values of classical
     # bits b1 and b2.
     bell_state_correction(destination_qubit, b1, b2)
-    print("After Bell State Correction:")
-    print(f"destination qubit: {destination_qubit.qstate}")
-
-    # Check whether the destination qubit has the same ket state as the copy of the source qubit, to
+    # Check whether the destination qubit has ket state that was chosed for the source qubit, to
     # check whether the teleportation was succesful. Note: this is impossible to check in real life,
     # but we are doing a simulation, so we can peek under the hood.
-    success = qubits_have_same_state(source_qubit_copy, destination_qubit)
-    print(f"success: {success}")
+    success = qubits_has_state(destination_qubit, source_state)
+    # Report result
+    report(f"Teleportation of qubit {source_state}: {'success' if success else 'FAILURE'}")
+    return success
 
 if __name__ == "__main__":
     ns.set_qstate_formalism(ns.QFormalism.KET)
-    teleport_one_qubit()
+    success_count = 0
+    fail_count = 0
+    for _ in range(1000):
+        if teleport_one_qubit():
+            success_count += 1
+        else:
+            fail_count += 1
+    print(f"{success_count} teleportations succeeded, {fail_count} teleportations failed")
